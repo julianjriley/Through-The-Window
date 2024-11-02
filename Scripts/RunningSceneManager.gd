c4c973c0e176edcc1335cea000609635f07a4cb8
Julian Riley <87546620+julianjriley@users.noreply.github.com>
2024-11-02 03:36:15 -0400

    Absolutely massive push
    All the things needed besides spike death.

---
+++ Scripts/RunningSceneManager.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	get_tree().paused = false
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_game_over_animation_restart_game() -> void:
+	get_tree().change_scene_to_file("res://Scenes/test_platform.tscn")
+	pass # Replace with function body.
+
+
+func _on_game_over_animation_main_menu() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+	pass # Replace with function body.
